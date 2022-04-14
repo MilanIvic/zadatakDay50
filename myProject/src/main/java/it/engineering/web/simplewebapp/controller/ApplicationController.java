34451909b020a1684158e60edfc364877cf8b50f
Milan Ivic <ivicmilan050@gmail.com>
2022-04-14 09:31:30 +0200

    empty project

---
+++ myProject/src/main/java/it/engineering/web/simplewebapp/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package it.engineering.web.simplewebapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApplicationController {
+
+	public String processRequest(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
